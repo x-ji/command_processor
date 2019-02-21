b14be4e5f9f329360d5e94b4f59bdecf8e18d6b0
Xiang Ji <hi@xiangji.me>
2019-02-21 23:38:56 +0100

    Add sanitize_tasks/1 in Task module

--- lib/command_processor/task.ex
+++ lib/command_processor/task.ex
@@ -13,4 +13,40 @@ defmodule CommandProcessor.Task do
     |> cast(attrs, [:name, :command, :requires])
     |> validate_required([:name, :command])
   end
+
+  @doc """
+  Sanitize and transform a list of tasks into a list of %Task structs.
+  """
+  def sanitize_tasks(tasks) when is_list(tasks) do
+    tasks =
+      tasks
+      |> Enum.map(&sanitize_task(&1))
+
+    all_valid =
+      Enum.reduce_while(tasks, true, fn t, acc ->
+        case t do
+          {:error, _} -> {:halt, false}
+          _ -> {:cont, acc}
+        end
+      end)
+
+    if all_valid do
+      # Get each task out of the {:ok, task} tuple.
+      {:ok, Enum.map(tasks, fn {:ok, task} -> task end)}
+    else
+      # Better stop the procedure if any input is invalid, instead of producing an incomplete (and potentially surprising) set of commands to the user.
+      :error
+    end
+  end
+
+  # If it's not a list then just return :error
+  def sanitize_tasks(_) do
+    :error
+  end
+
+  defp sanitize_task(task) do
+    %CommandProcessor.Task{}
+    |> CommandProcessor.Task.changeset(task)
+    |> Ecto.Changeset.apply_action(:insert)
+  end
 end
--- test/command_processor/task_test.exs
+++ test/command_processor/task_test.exs
@@ -2,7 +2,7 @@ defmodule CommandProcessor.TaskTest do
   use ExUnit.Case, async: true
   alias CommandProcessor.Task
 
-  @valid_attrs %{
+  @valid_task_attrs %{
     "name" => "task-4",
     "command" => "rm /tmp/file1",
     "requires" => [
@@ -11,32 +11,118 @@ defmodule CommandProcessor.TaskTest do
     ]
   }
 
-  @invalid_attrs %{}
+  @invalid_task_attrs %{}
+
+  @valid_tasks [
+    %{
+      "name" => "task-1",
+      "command" => "touch /tmp/file1"
+    },
+    %{
+      "name" => "task-2",
+      "command" => "cat /tmp/file1",
+      "requires" => [
+        "task-3"
+      ]
+    },
+    %{
+      "name" => "task-3",
+      "command" => "echo 'Hello World!' > /tmp/file1",
+      "requires" => [
+        "task-1"
+      ]
+    },
+    %{
+      "name" => "task-4",
+      "command" => "rm /tmp/file1",
+      "requires" => [
+        "task-2",
+        "task-3"
+      ]
+    }
+  ]
+
+  @invalid_tasks [
+    %{
+      "name" => "task-1",
+      "command" => "touch /tmp/file1"
+    },
+    %{
+      "name" => 1234,
+      "command" => "cat /tmp/file1",
+      "requires" => [
+        "task-3"
+      ]
+    }
+  ]
+
+  @sanitized_valid_tasks [
+    %Task{
+      name: "task-1",
+      command: "touch /tmp/file1"
+    },
+    %Task{
+      name: "task-2",
+      command: "cat /tmp/file1",
+      requires: [
+        "task-3"
+      ]
+    },
+    %Task{
+      name: "task-3",
+      command: "echo 'Hello World!' > /tmp/file1",
+      requires: [
+        "task-1"
+      ]
+    },
+    %Task{
+      name: "task-4",
+      command: "rm /tmp/file1",
+      requires: [
+        "task-2",
+        "task-3"
+      ]
+    }
+  ]
 
   describe "changeset/2" do
     test "changeset/2 with valid attributes" do
-      changeset = Task.changeset(%Task{}, @valid_attrs)
+      changeset = Task.changeset(%Task{}, @valid_task_attrs)
       assert changeset.valid?
     end
 
     test "changeset/2 with invalid attributes" do
-      changeset = Task.changeset(%Task{}, @invalid_attrs)
+      changeset = Task.changeset(%Task{}, @invalid_task_attrs)
       refute changeset.valid?
     end
 
     test ":name is required" do
-      changeset = Task.changeset(%Task{}, Map.delete(@valid_attrs, "name"))
+      changeset = Task.changeset(%Task{}, Map.delete(@valid_task_attrs, "name"))
       refute changeset.valid?
     end
 
     test ":command is required" do
-      changeset = Task.changeset(%Task{}, Map.delete(@valid_attrs, "command"))
+      changeset = Task.changeset(%Task{}, Map.delete(@valid_task_attrs, "command"))
       refute changeset.valid?
     end
 
     test ":requires is optional" do
-      changeset = Task.changeset(%Task{}, Map.delete(@valid_attrs, :requires))
+      changeset = Task.changeset(%Task{}, Map.delete(@valid_task_attrs, :requires))
       assert changeset.valid?
+    end
+  end
+
+  describe "sanitize_tasks/1" do
+    test "sanitize_tasks/1 produces list of sanitized tasks on valid list of task inputs" do
+      assert Task.sanitize_tasks(@valid_tasks) == {:ok, @sanitized_valid_tasks}
+    end
+
+    test "sanitize_tasks/1 produces :error on a list which contains invalid tasks" do
+      assert Task.sanitize_tasks(@invalid_tasks) == :error
+    end
+
+    test "sanitize_tasks/1 produces :error on non-list input" do
+      assert Task.sanitize_tasks("notalist") == :error
     end
   end
 end
