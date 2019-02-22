5030299913d4f2e0efd6562a97d11bda531d5dac
Xiang Ji <hi@xiangji.me>
2019-02-22 01:00:30 +0100

    Add libgraph as a dependency

--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule CommandProcessor.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:ecto, "~> 3.0"}
+      {:ecto, "~> 3.0"},
+      {:libgraph, "~> 0.13.3"}
     ]
   end
 end
